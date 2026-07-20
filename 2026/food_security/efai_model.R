3d1e031ef07d6292d4dc17837afc58bfc99e1d6b
Anne Driscoll <driscollannem@gmail.com>
2026-07-20 16:57:38 -0400

    Update efai_model.R

--- 2026/food_security/efai_model.R
+++ 2026/food_security/efai_model.R
@@ -1,4 +1,4 @@
-
+source("../../../tokens.R")
 library(tidyverse)
 library(tidycensus)
 library(sf)
@@ -6,6 +6,9 @@ library(jsonlite)
 library(readxl)
 library(corrplot)
 library(leaps)
+library(broom)
+library(purrr)
+census_api_key(census_key)
 
 ################################################################################
 # read efai + food insecurity data
@@ -110,17 +113,20 @@ combined = efai %>% dplyr::select(GEOID, index) %>%
          bodega_10k = Bodega/total_popE*10000, 
          fastfood_10k = `Fast Food`/total_popE*10000, 
          supply_gap_lbs_natural = -supply_gap_lbs, 
+         supply_gap_lbs_10k = -supply_gap_lbs/10000, 
          majority_race = case_when(perc_white > perc_black & perc_white > perc_asian ~ "white", 
                                    perc_black > perc_white & perc_black > perc_asian ~ "black", 
                                    perc_asian > perc_white & perc_asian > perc_black ~ "asian"), 
          majority_eth = case_when(perc_white > perc_black & perc_white > perc_asian & perc_white > perc_hisp ~ "white", 
                                    perc_black > perc_white & perc_black > perc_asian & perc_black > perc_hisp ~ "black", 
                                    perc_asian > perc_white & perc_asian > perc_black & perc_asian > perc_hisp  ~ "asian", 
-                                   perc_hisp > perc_white & perc_hisp > perc_black & perc_hisp > perc_asian  ~ "hisp")) %>% 
+                                   perc_hisp > perc_white & perc_hisp > perc_black & perc_hisp > perc_asian  ~ "hisp"), 
+         income_quantile = ecdf(median_incomeE)(median_incomeE),
+         income_tercile = factor(ntile(median_incomeE, 3), labels = c("low", "mid", "high")),
+         majority_eth = relevel(factor(majority_eth), ref = "white"), 
+         majority_race = relevel(factor(majority_race), ref = "white"), 
+         pop_1k = total_popE/1000) %>% 
   filter(total_popE > 0)
-
-combined$majority_eth = relevel(factor(combined$majority_eth), ref = "white")
-combined$majority_race = relevel(factor(combined$majority_race), ref = "white")
 
 
 ################################################################################
@@ -133,12 +139,12 @@ corrplot(cor(combined %>%
                       grocery_10k, bodega_10k, vices_10k, fastfood_10k)), 
          method = 'number')
 
-# how much access to emergency food is there (+ is GOOD)
+# how much access to emergency food is there (+ = more access)
 summary(lm(index ~ total_popE + perc_black + perc_hisp + 
              food_insecure_percentage + supply_gap_lbs_natural, 
            data = combined))
 
-# does the area majority impact the food insecurity index?
+# does the area majority impact the food insecurity index? (+ = more access)
 summary(lm(index ~ total_popE + majority_eth + food_insecure_percentage + supply_gap_lbs_natural, 
            data = combined))
 
@@ -160,3 +166,52 @@ models = regsubsets(index ~ total_popE + median_incomeE + perc_pov + perc_white 
                 grocery_10k + bodega_10k + vices_10k + fastfood_10k, 
                data = combined, nbest = 1, nvmax = 15, method="exhaustive")
 summary(models)
+
+
+################################################################################
+# plot models
+################################################################################
+
+# run models
+formulas = c("index ~ pop_1k + perc_black + perc_hisp + perc_asian + food_insecure_percentage + supply_gap_lbs_10k",
+             "index ~ pop_1k + perc_black + perc_hisp + food_insecure_percentage + supply_gap_lbs_10k",
+             "index ~ pop_1k + majority_eth + food_insecure_percentage + supply_gap_lbs_10k",
+             "index ~ pop_1k + majority_eth*food_insecure_percentage + supply_gap_lbs_10k",
+             "index ~ pop_1k + majority_eth*income_tercile + supply_gap_lbs_10k",
+             "index ~ pop_1k + majority_eth*supply_gap_lbs_10k")
+models = lapply(formulas, function(f) lm(as.formula(f), data = combined))
+names(models) = formulas  # use the formula string as the model label
+
+# get coefs
+coef_df = imap_dfr(models, function(mod, mod_name) {
+  tidy(mod, conf.int = TRUE) %>%
+    mutate(model = mod_name)
+}) %>% mutate(term = fct_relevel(term, "(Intercept)", "pop_1k", 
+                                 "food_insecure_percentage", "supply_gap_lbs_10k", 
+                                 "income_tercilemid", "income_tercilehigh",
+                                 "perc_black", "majority_ethblack", "majority_ethblack:income_tercilemid", 
+                                    "majority_ethblack:income_tercilehigh","majority_ethblack:food_insecure_percentage",
+                                    "majority_ethblack:supply_gap_lbs_10k", 
+                                 "perc_hisp", "majority_ethhisp", "majority_ethhisp:income_tercilemid", 
+                                    "majority_ethhisp:income_tercilehigh", "majority_ethhisp:food_insecure_percentage", 
+                                    "majority_ethhisp:supply_gap_lbs_10k", 
+                                 "perc_asian", "majority_ethasian", "majority_ethasian:income_tercilemid", 
+                                    "majority_ethasian:income_tercilehigh", "majority_ethasian:food_insecure_percentage", 
+                                    "majority_ethasian:supply_gap_lbs_10k"))
+
+#coef_df <- coef_df %>% filter(term != "(Intercept)")
+
+# plot
+dodge_width <- 0.6
+ggplot(coef_df, aes(x = fct_rev(term), y = estimate, color = model, group = model, alpha = `p.value` < 0.05)) +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "grey50") +
+  geom_errorbar(aes(ymin = conf.low, ymax = conf.high),
+                position = position_dodge(width = dodge_width), width = 0.15) +
+  geom_point(position = position_dodge(width = dodge_width), size = 2.5) +
+  coord_flip() +
+  labs(x = NULL, y = "Coefficient estimate", color = "Model",
+       title = "Model coefficients with 95% CIs") +
+  theme_minimal(base_size = 13) +
+  theme(panel.grid.minor = element_blank(), legend.position = "bottom") +
+  scale_alpha_manual(values = c(`TRUE` = 1, `FALSE` = 0.35))
+
